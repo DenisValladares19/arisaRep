1983d8820fb55bc3a7769f91afe0ded3ff76f73d
Admin <Admin@Admin-PC>
2019-11-21 18:57:36 -0600

    primer commit

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JC
+ * Date: 21/11/2019
+ * Time: 15:53
+ */
+
+defined("BASEPATH");
+class Dashboard extends CI_Controller
+{
+    public function __construct()
+    {
+
+        parent::__construct();
+        $this->load->helper("url");
+    }
+
+    public function index(){
+
+        $this->load->view("layout/header");
+        $this->load->view("layout/sidebar");
+        $this->load->view("layout/navbar");
+        $this->load->view("dashboard/index_dashboard");
+        $this->load->view("layout/footer");
+
+    }
+
+}
